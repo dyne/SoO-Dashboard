ae679a65a93cd8028f79312b29d7401de8ecd4cc
phoebus-84 <ennio@dyne.org>
2022-05-13 13:26:43 +0200

    add services page with mock services

---
+++ mocks/services.ts
@@ -0,0 +1,30 @@
+const ServicesMock: Array<{name:string, url:string, region:string}> = [
+  {
+    name: "nome1",
+    url: "http://12345.12/url1",
+    region: "EstEu"
+  },
+  {
+    name: "nome2",
+    url: "http://12345.12/url2",
+    region: "EstEu"
+  },
+  {
+    name: "nome3",
+    url: "http://12345.12/url3",
+    region: "EstEu"
+  },
+  {
+    name: "nome4",
+    url: "http://12345.12/url4",
+    region: "EstEu"
+  },
+  {
+    name: "nome5",
+    url: "http://12345.12/url5",
+    region: "EstEu"
+  }
+];
+console.log(ServicesMock)
+
+export default ServicesMock
