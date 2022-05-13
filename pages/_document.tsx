04dc3d47f6f3f1f63972643f786f4203ba75d2fb
phoebus-84 <ennio@dyne.org>
2022-05-13 10:41:52 +0200

    add layout, homepage identities page

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Layout from '../components/layout'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='crossorigin'/>
+            <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Layout>
+            <Main />
+          </Layout>
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
