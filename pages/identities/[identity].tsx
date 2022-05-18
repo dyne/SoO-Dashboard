0a260568d9fdd7e8830cfd9c15b6e9de2ca6f054
phoebus-84 <ennio@dyne.org>
2022-05-18 11:48:37 +0200

    fix dark mode identity page

--- pages/identities/[identity].tsx
+++ pages/identities/[identity].tsx
@@ -16,7 +16,7 @@ const Identity: NextPage = () => {
             <h1 className="text-2xl">{uid}</h1>
             {data && <>
                 <h2 className="text-xl font-bold">Blockchain addresses and PKs</h2>
-                <div className="border-t border-gray-200">
+                <div className="border-t border-gray-600">
                     <dl>
                         <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500">ethereum</dt>
@@ -24,15 +24,15 @@ const Identity: NextPage = () => {
                         </div>
                         <div className="px-4 py-5 break-all sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="overflow-auto text-sm font-medium text-gray-500">reflow public key</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data?.identity.reflow_public_key}</dd>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{data?.identity.reflow_public_key}</dd>
                         </div>
                         <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500">ecdh public key</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data?.identity.ecdh_public_key}</dd>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{data?.identity.ecdh_public_key}</dd>
                         </div>
                         <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500">ecdh public key</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data?.identity.ecdh_public_key}</dd>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{data?.identity.ecdh_public_key}</dd>
                         </div>
                         <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500">bitcoin</dt>
