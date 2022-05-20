bdfc99b985d329ceba3f9b77654089be4f57198b
phoebus-84 <ennio@dyne.org>
2022-05-20 17:26:24 +0200

    better tooltip in identitities subscripons

--- components/SubscriptionsCell.tsx
+++ components/SubscriptionsCell.tsx
@@ -1,6 +1,14 @@
 import useSWR from "swr";
 import React from "react";
 
+const SubscriptionTooltip = ({ value }: { value: any}) => {
+    const valueKeys = Object.keys(value)
+    return <>{valueKeys.map((v,i)=><div key={i} className="tooltip" data-tip={value[v]}>
+        <a className="text-xs mr-2 bold">{v}</a>
+    </div>)}</>
+
+}
+
 const SubscriptionsCell = ({ uid }: { uid: string }) => {
     const url = `http://${uid}/api/zenswarm-oracle-get-identity`
     const { data } = useSWR(url)
@@ -8,9 +16,9 @@ const SubscriptionsCell = ({ uid }: { uid: string }) => {
     const subscriptionsKeys =Object.keys(subscriptions? subscriptions : {})
 
     return (<>{subscriptionsKeys && subscriptionsKeys.map((s,i)=>
-                <div key={i} className="tooltip" data-tip={JSON.stringify(subscriptions[s])}>
-                    <span>{s}</span>
-                </div>)}
+            <><span key={i}>{s}</span><br/>
+        <SubscriptionTooltip value={subscriptions[s]}/>
+              </>  )}
             </>)
 }
 
