f9580f7d6c200ca5981ecdfc445e85fa18c18ce9
Puria Nafisi Azizi <puria@dyne.org>
2022-05-21 05:32:07 +0200

    chore: Add link to the software passport

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type { NextPage } from 'next';
-import { useSession } from "next-auth/react";
+import type {NextPage} from 'next';
+import {useSession} from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
-type HomepageData = {title: string, text:string, href:string, disabled?:boolean}
+type HomepageData = {title: string, text: string, href: string, disabled?: boolean}
 
 
-function Card({title,text, href, disabled=false}: HomepageData) {
-    const link = disabled? "/" : href
+function Card({title, text, href, disabled = false}: HomepageData) {
+    const link = disabled ? "/" : href
     return (
         <Link href={href}>
             <div className="card w-full h-60 mx-4 my-1 drop-shadow-xl bg-base-100">
                 <div className="card-body">
                     <h2 className="card-title font-bold text-l">{title}</h2>
-                     <p className="text-gray-400">{text}</p>
+                    <p className="text-gray-400">{text}</p>
                     <div className="card-actions justify-end">
                         <Link href={link}><button className="btn btn-primary" disabled={disabled}>Go</button></Link>
                     </div>
@@ -24,17 +24,17 @@ function Card({title,text, href, disabled=false}: HomepageData) {
 
 
 const Home: NextPage = () => {
-    const { data: session } = useSession()
+    const {data: session} = useSession()
 
-    const homepageData:Array<HomepageData>  = [
+    const homepageData: Array<HomepageData> = [
         {
             title: 'L0',
             href: '/l0',
             text: 'Layer 0 monitoring'
         },
         {
-            title: 'SoftwarePassport Task',
-            href: '/',
+            title: 'Software Passport',
+            href: 'http://softwarepassport.dyne.org:3000',
             text: ''
         },
         {
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
         },
         {
             title: 'Services',
-            href: session? '/services' : '/',
+            href: session ? '/services' : '/',
             text: 'Monitor active services',
             disabled: !!!session
         },
@@ -65,7 +65,7 @@ const Home: NextPage = () => {
             <div>
                 <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                     <div className="columns-1 md:columns-2">
-                        {homepageData.map((h,i)=> <Card {...h} key={i}/>)}
+                        {homepageData.map((h, i) => <Card {...h} key={i} />)}
                     </div>
                 </div>
             </div>
