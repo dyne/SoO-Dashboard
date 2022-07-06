4e757e708b421bd0c7bd5eea30b8a636113a11a9
phoebus-84 <ennio@dyne.org>
2022-07-06 11:44:22 +0200

    oraacles consesus post

--- pages/oracles_consesus_post.tsx
+++ pages/oracles_consesus_post.tsx
@@ -1,25 +1,38 @@
-import { NextPage } from "next";
-import React, { useState } from "react";
+import {NextPage} from "next";
+import React, {useState} from "react";
 import axios from "axios";
+import useSWR from "swr";
 
 
 const OraclesConsensusPost: NextPage = () => {
+    const {data} = useSWR("https://did.dyne.org:443/api/W3C-DID-extract-oracles-uid.chain")
+    const urlPlaceholder = (data && data['W3C-DIDs_uid_list']) && `${data['W3C-DIDs_uid_list'][0]}/api/zenswarm-post-6-rand-oracles.chain`
+    const [endpoint, setEndpoint] = useState('https://swarm1.dyne.org:20003/api/zenswarm-post-6-rand-oracles.chain')
+    const handleSelect = (e: any) => {
+        setEndpoint(`${e.target.value}/api/zenswarm-post-6-rand-oracles.chain`)
+    }
+    const postPlaceholder = `{
+        "data": {
+            "txId": "94d5a744e8a4d8153cda90a900fdeba5b763a945411f3f7ebe33c7d54b91bbf0",
+            "post": {
+                "data": {
+                    "post": {
+                        "id": "01FSKCT2BR8326ZEM6F6C4STWM",
+                        "recurseLimit": 2
+                    },
+                    "endpoint": "https://reflow-demo.dyne.org/api/json/trace"
+                }
+            }
+        }
+    }`
 
-    const placeholder = `{
-        "post": {
-                "id": "01FSKCT2BR8326ZEM6F6C4STWM",
-               "recurseLimit": 2
-            },
-            "endpoint": "https://reflow-demo.dyne.org/api/json/trace"
-        }`
-
-    const [post, setPost] = useState(placeholder)
+    const [post, setPost] = useState(postPlaceholder)
     const [response, setResponse] = useState(null as any)
 
     function consesusPost(e: any) {
         e.preventDefault()
-        const input = { data: { "post": { data: JSON.parse(post) } } }
-        axios.post('https://apiroom.net/api/zenswarm/zenswarm-6-random-oracles-post.chain', input, {
+        const input = JSON.parse(post)
+        axios.post(endpoint, input, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
@@ -28,20 +41,31 @@ const OraclesConsensusPost: NextPage = () => {
             .then((res) => setResponse(res.data))
             .catch((error) => setResponse(error));
     }
+
     const title: string = 'Oracles Consensus Post'
 
     return (<>
         <h1 className="pb-4 text-2xl font-bold">{title}</h1>
-        <div className="grid max-h-screen grid-cols-1 md:grid-cols-2">
-            <form onSubmit={consesusPost}>
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-3">
+            <form className="grid grid-cols-1 md:col-span-7" onSubmit={consesusPost}>
+                <select className="select select-bordered my-2" onChange={((e: any) => handleSelect(e))}>
+                    {data && data['W3C-DIDs_uid_list']?.map((n: string) =>
+                        <option key={n} value={n}>
+                            {n}
+                        </option>)}
+                </select>
+                <h5 className="font-bold">
+                    EndPoint:<br/><span className="text-gray-400">{endpoint}</span>
+                </h5>
                 <textarea
-                    placeholder={placeholder}
+                    placeholder={postPlaceholder}
                     onChange={(e) => setPost(e.target.value)}
-                    className="w-4/5 mb-2 input textarea-bordered h-60 textarea" /><br />
+                    className="mb-2 input textarea-bordered h-60 textarea"/><br/>
                 <button type="submit"
-                    className="btn btn-primary">Post!</button>
+                        className="btn btn-primary">Post!
+                </button>
             </form>
-            {response && <pre className="p-4 overflow-auto bg-black text-accent whitespace-pre">
+            {response && <pre className="p-4 overflow-auto bg-black text-accent whitespace-pre md:col-span-5">
                 {JSON.stringify(response, null, 2)}
             </pre>}
         </div>
